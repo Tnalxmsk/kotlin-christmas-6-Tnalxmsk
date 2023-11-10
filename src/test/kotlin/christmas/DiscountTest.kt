336bce4e603600641c0eb57cab3d2ef35769bd79
Tnalxmsk <alstnwkd990@naver.com>
2023-11-10 22:47:40 +0900

    test(christmas): Discount Test class 추가
    크리스마스 디데이 할인 이벤트 동안과 종료 후 할인 적용 기능 테스트

---
+++ src/test/kotlin/christmas/DiscountTest.kt
@@ -0,0 +1,23 @@
+package christmas
+
+import christmas.model.Discount
+import christmas.model.Order
+import christmas.model.VisitDate
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+
+class DiscountTest {
+    @Test
+    fun `크리스마스 디데이 할인 적용 확인`() {
+        val testDate = VisitDate(24)
+        val testOrder = Order(mutableMapOf())
+        assertThat(Discount(testDate, testOrder).applyDDayDiscount()).isEqualTo(3300)
+    }
+
+    @Test
+    fun `25일 이후 디데이 할인 적용이 끝났는지 확인`() {
+        val testDate = VisitDate(27)
+        val testOrder = Order(mutableMapOf())
+        assertThat(Discount(testDate, testOrder).applyDDayDiscount()).isEqualTo(0)
+    }
+}
