bf9953f49f90ec4bb7bb282413425233a2dbf2ee
Tnalxmsk <alstnwkd990@naver.com>
2023-11-11 06:20:38 +0900

    feat(christmas): Order class 기능 추가
    에피타이저, 음료가 주문 목록에 있는지 확인하는 기능과 에피타이저와, 음료 주문만 반환하는 기능 구현

--- src/main/kotlin/christmas/model/Order.kt
+++ src/main/kotlin/christmas/model/Order.kt
@@ -1,17 +1,24 @@
 package christmas.model
 
-import christmas.model.menu.Dessert
-import christmas.model.menu.MainDish
-import christmas.model.menu.Menu
+import christmas.model.menu.*
 
 class Order(private val orderMenus: List<Menu>) {
     private fun isDessert(orderMenu: String): Boolean = Dessert.entries.any { it.menuName.contains(orderMenu) }
 
     private fun isMainDish(orderMenu: String): Boolean = MainDish.entries.any { it.menuName.contains(orderMenu) }
 
+    private fun isAppetizer(orderMenu: String): Boolean = Appetizer.entries.any { it.menuName.contains(orderMenu) }
+
+    private fun isBeverage(orderMenu: String): Boolean = Beverage.entries.any { it.menuName.contains(orderMenu) }
+
     fun findDessert(): List<Menu> = orderMenus.filter { menu -> isDessert(menu.menuName) }
 
     fun findMainDish(): List<Menu> = orderMenus.filter { menu -> isMainDish(menu.menuName) }
+
+    fun findAppetizer(): List<Menu> = orderMenus.filter { menu -> isAppetizer(menu.menuName) }
+
+    fun findBeverage(): List<Menu> = orderMenus.filter { menu -> isBeverage(menu.menuName) }
+
 
     fun getOrderMenus(): List<Menu> = orderMenus
 }
