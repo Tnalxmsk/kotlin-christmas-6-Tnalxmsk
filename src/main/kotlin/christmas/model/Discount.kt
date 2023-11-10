f7724ca42c7ff12ef1174f2021b739644deceb3d
Tnalxmsk <alstnwkd990@naver.com>
2023-11-10 23:17:33 +0900

    refactor(christmas): D-day 할인 기능 수정
    Discount 생성 시점에 할인이 적용되도록 수정
    d-day 할인과 총 할인 금액을 반환하는 기능 추가

--- src/main/kotlin/christmas/model/Discount.kt
+++ src/main/kotlin/christmas/model/Discount.kt
@@ -2,12 +2,23 @@ package christmas.model
 
 class Discount(private val date: VisitDate, private val order: Order) {
     private var totalDiscount = 0
-    fun applyDDayDiscount(): Int {
-        if (date.visitDate > 25) return NOT_DISCOUNT_D_DAY
-        val dDayDiscount = DEFAULT_D_DAY_DISCOUNT + (PLUS_D_DAY_DISCOUNT * (date.visitDate - 1))
-        totalDiscount += dDayDiscount
-        return dDayDiscount
+    private var dDayDiscount = 0
+    init {
+        applyDDayDiscount()
     }
+
+    private fun applyDDayDiscount() {
+        if (date.getVisitDate() > 25) {
+            dDayDiscount =  NOT_DISCOUNT_D_DAY
+            return
+        }
+        dDayDiscount = DEFAULT_D_DAY_DISCOUNT + (PLUS_D_DAY_DISCOUNT * (date.getVisitDate() - 1))
+        totalDiscount += dDayDiscount
+    }
+
+    fun getDDayDiscount(): Int = dDayDiscount
+
+    fun getTotalDiscount(): Int = totalDiscount
 
     companion object {
         private const val DEFAULT_D_DAY_DISCOUNT = 1000
