a8856dab43fba9fc438d0d33e568a724e43b3aac
Tnalxmsk <alstnwkd990@naver.com>
2023-11-11 03:39:58 +0900

    test(christmas): 평일 할인이 적용되었는지 확인하는 테스트 코드 추가

--- src/test/kotlin/christmas/DiscountTest.kt
+++ src/test/kotlin/christmas/DiscountTest.kt
@@ -20,4 +20,15 @@ class DiscountTest {
         val testOrder = Order(mutableMapOf())
         assertThat(Discount(testDate, testOrder).getDDayDiscount()).isEqualTo(0)
     }
+
+    @Test
+    fun `평일 할인이 적용되는지 확인`() {
+        val menus = mutableMapOf<String, Int>()
+        menus["초코케이크"] = 1
+        menus["아이스크림"] = 2
+        val testDate = VisitDate(27)
+        val testOrder = Order(menus)
+        val discount = Discount(testDate, testOrder)
+        assertThat(discount.getWeekdayDiscount()).isEqualTo(6069)
+    }
 }
