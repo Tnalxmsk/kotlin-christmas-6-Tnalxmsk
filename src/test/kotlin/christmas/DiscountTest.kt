ed3f7f6ddce3e73df5cba0f5ff83d3ea9ed4af97
Tnalxmsk <alstnwkd990@naver.com>
2023-11-11 03:53:58 +0900

    test(christmas): 주말 할인 기능 테스트 코드 추가

--- src/test/kotlin/christmas/DiscountTest.kt
+++ src/test/kotlin/christmas/DiscountTest.kt
@@ -31,4 +31,15 @@ class DiscountTest {
         val discount = Discount(testDate, testOrder)
         assertThat(discount.getWeekdayDiscount()).isEqualTo(6069)
     }
+
+    @Test
+    fun `주말 할인이 적용되었는지 확인`() {
+        val menus = mutableMapOf<String, Int>()
+        menus["티본스테이크"] = 3
+        menus["크리스마스파스타"] = 2
+        val testDate = VisitDate(27)
+        val testOrder = Order(menus)
+        val discount = Discount(testDate, testOrder)
+        assertThat(discount.getWeekendDiscount()).isEqualTo(6069+4046)
+    }
 }
