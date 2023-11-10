1542b7fec5d7ac9da8a2dc7ad139ed6953d367cb
Tnalxmsk <alstnwkd990@naver.com>
2023-11-11 06:14:43 +0900

    feat(christmas): enum class 안의 메뉴 이름을 찾아주는 기능 추가
    4가지 카테고리의 메뉴 모두 추가

--- src/main/kotlin/christmas/model/menu/Appetizer.kt
+++ src/main/kotlin/christmas/model/menu/Appetizer.kt
@@ -4,4 +4,8 @@ enum class Appetizer(val menuName: String, val price: Int) {
     SOUP("양송이수프", 6_000),
     TAPAS("타파스", 5_500),
     SALAD("시저샐러드", 8000);
+
+    companion object {
+        fun findByMenuName(menuName: String): Appetizer? = Appetizer.entries.find { it.menuName == menuName }
+    }
 }
--- src/main/kotlin/christmas/model/menu/MainDish.kt
+++ src/main/kotlin/christmas/model/menu/MainDish.kt
@@ -4,5 +4,9 @@ enum class MainDish(val menuName: String, val price: Int) {
     T_BONE_STEAK("티본스테이크", 55000),
     BBQ_RIBS("바비큐립", 54000),
     SEAFOOD_PASTA("해산물파스타", 35000),
-    CHRISTMAS_PASTA("크리스마스파스타", 25000)
+    CHRISTMAS_PASTA("크리스마스파스타", 25000);
+
+    companion object {
+        fun findByMenuName(menuName: String): MainDish? = MainDish.entries.find { it.menuName == menuName }
+    }
 }
