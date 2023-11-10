372c653b1916955c6a36ba1f083f9552342901da
Tnalxmsk <alstnwkd990@naver.com>
2023-11-11 03:55:10 +0900

    feat(christmas): 메인 메뉴와 관련된 기능 추가
    메인 메뉴인지 확인하는 isMainDish()
    주문 목록에서 메인 메뉴를 반환하는 findMainDish()

--- src/main/kotlin/christmas/model/Order.kt
+++ src/main/kotlin/christmas/model/Order.kt
@@ -1,13 +1,16 @@
 package christmas.model
 
 import christmas.model.menu.Dessert
+import christmas.model.menu.MainDish
 
 class Order(private val orderMenus: Map<String, Int>) {
-    private fun isDessert(orderMenu: String): Boolean{
-        return Dessert.entries.any { it.menuName.contains(orderMenu) }
-    }
+    private fun isDessert(orderMenu: String): Boolean = Dessert.entries.any { it.menuName.contains(orderMenu) }
 
-    fun findDessert(): Map<String, Int> = orderMenus.filter { isDessert(it.key) }
+    private fun isMainDish(orderMenu: String): Boolean = MainDish.entries.any { it.menuName.contains(orderMenu) }
+
+    fun findDessert(): Map<String, Int> = orderMenus.filter { menu -> isDessert(menu.key) }
+
+    fun findMainDish(): Map<String, Int> = orderMenus.filter { menu -> isMainDish(menu.key) }
 
     fun getOrderMenus(): Map<String, Int> = orderMenus
 }
