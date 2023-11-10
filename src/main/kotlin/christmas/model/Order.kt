ea3250aab2768fb45b3fc23cf5143b19201d9637
Tnalxmsk <alstnwkd990@naver.com>
2023-11-11 03:18:59 +0900

    feat(christmas): Order Class isDessert(), findDessert 메서드 추가
    enum class Desssrt에 포함되어 있는지 확인하는 메서드 isDessert()
    주문 목록 중 Dessert 메뉴를 반환하는 findDessert()

--- src/main/kotlin/christmas/model/Order.kt
+++ src/main/kotlin/christmas/model/Order.kt
@@ -1,5 +1,13 @@
 package christmas.model
 
-data class Order(
-    val orderMenus: Map<String, Int>
-)
+import christmas.model.menu.Dessert
+
+class Order(private val orderMenus: Map<String, Int>) {
+    private fun isDessert(orderMenu: String): Boolean{
+        return Dessert.entries.any { it.menuName.contains(orderMenu) }
+    }
+
+    fun findDessert(): Map<String, Int> = orderMenus.filter { isDessert(it.key) }
+
+    fun getOrderMenus(): Map<String, Int> = orderMenus
+}
