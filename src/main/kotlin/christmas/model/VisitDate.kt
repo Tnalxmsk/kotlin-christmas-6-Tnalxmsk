5abd98b2c96ec08d4d478082ba99eeba551bee82
Tnalxmsk <alstnwkd990@naver.com>
2023-11-11 04:09:45 +0900

    feat(christmas): VisitDate 평일, 주말, 크리스마스 D-DAY 이벤트 기간, 특별 할인 확인 기능 추가

--- src/main/kotlin/christmas/model/VisitDate.kt
+++ src/main/kotlin/christmas/model/VisitDate.kt
@@ -1,11 +1,28 @@
 package christmas.model
 
 class VisitDate(private val visitDate: Int) {
+    // 각 날짜를 출력 추후 삭제
     fun getVisitDate() = visitDate
 
     fun isWeekday(): Boolean {
-        val weekDay = (1..31) - (1..30 step 7)
+        val weekDay = (1..31) - (1..31 step 7) - (2..31 step 7)
         println(weekDay)
         return weekDay.contains(visitDate)
+    }
+
+    fun isWeekend(): Boolean {
+        val weekend = (1..31 step 7) + (2..31 step 7)
+        println(weekend)
+        return weekend.contains(visitDate)
+    }
+
+    fun isSpecialDay(): Boolean {
+        val specialDay = (3..31 step 7) + 25
+        println(specialDay)
+        return specialDay.contains(visitDate)
+    }
+
+    fun isChristmasDDayEvent(): Boolean {
+        return visitDate < 25
     }
 }
