c24f500c568964af63e76e34c41b30f215487ae3
Tnalxmsk <alstnwkd990@naver.com>
2023-11-13 04:22:33 +0900

    feat(christmas): 날짜 입력 공백 포함 시 예외 추가

--- src/main/kotlin/christmas/model/EventPresentation.kt
+++
@@ -1,7 +0,0 @@
-package christmas.model
-
-object EventPresentation {
-    fun checkEventCondition(beforeDiscountTotalPrice: Int): Boolean {
-        return beforeDiscountTotalPrice >= 120_000
-    }
-}
--- src/main/kotlin/christmas/validation/DateValidator.kt
+++ src/main/kotlin/christmas/validation/DateValidator.kt
@@ -2,15 +2,15 @@ package christmas.validation
 
 enum class DateValidator(val error: String) {
     EMPTY_DATE("[ERROR] 날짜를 입력하지 않았습니다. 다시 입력해 주세요."),
-    INCLUDE_SPACE("[ERROR] 공백이 포함되어 있습니다. 다시 입력해 주세요."),
-    INCLUDE_CHARACTER("[ERROR] 날짜는 숫자만 입력 가능합니다. 다시 입력해 주세요."),
+    INCLUDE_GAP("[ERROR] 공백이 포함되어 있습니다. 다시 입력해 주세요."),
+    INCLUDE_CHARACTER("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
     OUT_OF_RANGE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
 
     companion object {
         fun validateDate(input: String) {
             val error = when {
                 input.isEmpty() -> EMPTY_DATE
-                input.contains(" ") -> INCLUDE_SPACE
+                input.contains(" ") -> INCLUDE_GAP
                 input.matches(Regex("\\d+")).not() -> INCLUDE_CHARACTER
                 input.toInt() !in (1..31) -> OUT_OF_RANGE
                 else -> return
