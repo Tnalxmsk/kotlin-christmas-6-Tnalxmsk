77cae03ad6e9f21499f76ef65f9cf7afcc33f83f
Tnalxmsk <alstnwkd990@naver.com>
2023-11-12 15:44:08 +0900

    refactor(christmas): VisitDate 코드 상수화

--- src/main/kotlin/christmas/model/VisitDate.kt
+++ src/main/kotlin/christmas/model/VisitDate.kt
@@ -4,21 +4,32 @@ class VisitDate(private val visitDate: Int) {
     fun getVisitDate() = visitDate
 
     fun isWeekday(): Boolean {
-        val weekDay = (1..31) - (1..31 step 7) - (2..31 step 7)
+        val weekDay = (FIRST_DAY..LAST_DAY) -
+                (FIRST_DAY..LAST_DAY step WEEK) -
+                (SECOND_DAY..LAST_DAY step WEEK)
         return weekDay.contains(visitDate)
     }
 
     fun isWeekend(): Boolean {
-        val weekend = (1..31 step 7) + (2..31 step 7)
+        val weekend = (FIRST_DAY..LAST_DAY step WEEK) + (SECOND_DAY..LAST_DAY step WEEK)
         return weekend.contains(visitDate)
     }
 
     fun isSpecialDay(): Boolean {
-        val specialDay = (3..31 step 7) + 25
+        val specialDay = (THIRD_DAY..LAST_DAY step WEEK) + CHRISTMAS
         return specialDay.contains(visitDate)
     }
 
     fun isChristmasDDayEvent(): Boolean {
-        return visitDate < 25
+        return visitDate < CHRISTMAS
+    }
+
+    companion object {
+        private const val FIRST_DAY = 1
+        private const val SECOND_DAY = 2
+        private const val THIRD_DAY = 3
+        private const val LAST_DAY = 31
+        private const val WEEK = 7
+        private const val CHRISTMAS = 25
     }
 }
