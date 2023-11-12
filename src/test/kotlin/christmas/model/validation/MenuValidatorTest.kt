8864f7540cef01da43da4c8f7e8fade2cbc6e664
Tnalxmsk <alstnwkd990@naver.com>
2023-11-13 04:33:54 +0900

    test(christmas): 메뉴 입력 테스트 코드 추가

---
+++ src/test/kotlin/christmas/model/validation/MenuValidatorTest.kt
@@ -0,0 +1,31 @@
+package christmas.model.validation
+
+import christmas.validation.MenuValidator
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+
+class MenuValidatorTest {
+    @Test
+    fun `중복 요소가 있으면 true를 반환하는지 확인`() {
+        val input = "스테이크-1,스테이크-2,샴페인-3,제로콜라-2".split(",")
+        assertThat(MenuValidator.hasDuplicationMenu(input)).isEqualTo(true)
+    }
+
+    @Test
+    fun `주문 개수가 20개가 넘으면 true를 반환하는지 확인`() {
+        val input = "스테이크-1,스테이크-2,물-25,제로콜라-10".split(",")
+        assertThat(MenuValidator.isOverCount(input)).isEqualTo(true)
+    }
+
+    @Test
+    fun `주문 개수에 문자열이 포함되어 있으면 true를 반환하는지 확인`() {
+        val input = "스테이크-1,제로콜라-a".split(",")
+        assertThat(MenuValidator.hasCountCharacter(input)).isEqualTo(true)
+    }
+
+    @Test
+    fun `-이 없으면 true를 반환하는지 확인`() {
+        val input = "스테이크-1,제로콜라a".split(",")
+        assertThat(MenuValidator.isNotContainHyphen(input)).isEqualTo(true)
+    }
+}
