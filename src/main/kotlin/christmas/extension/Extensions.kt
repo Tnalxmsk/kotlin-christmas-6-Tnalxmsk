67454bb80badb193593818ac4eb2c4a1a1ead7eb
Tnalxmsk <alstnwkd990@naver.com>
2023-11-13 15:38:39 +0900

    feat(christmas): 확장 함수 추가
    입력 받은 스트링 메뉴를 리스트로 변환하는 확장함수 추가

--- src/main/kotlin/christmas/extension/Extensions.kt
+++ src/main/kotlin/christmas/extension/Extensions.kt
@@ -2,6 +2,10 @@ package christmas.extension
 
 import christmas.model.menu.Menu
 
+fun String.toStringList(): List<String> {
+    return this.split(",")
+}
+
 fun String.toStringToMenuList(): List<Menu> {
     val orders = this.split(",")
     return orders.map { order ->
