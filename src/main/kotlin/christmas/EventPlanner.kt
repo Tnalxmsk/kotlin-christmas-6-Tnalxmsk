3516a7a0309d513537ecdd7daad271f4c1ca18e9
Tnalxmsk <alstnwkd990@naver.com>
2023-11-14 23:16:26 +0900

    refactor(christmas): 모든 outputView를 출력하는 함수를 추가
    startPlanner 코드 개선

--- src/main/kotlin/christmas/EventPlanner.kt
+++ src/main/kotlin/christmas/EventPlanner.kt
@@ -1,5 +1,6 @@
 package christmas
 
+import christmas.model.Order
 import christmas.model.discount.DecemberDiscount
 import christmas.model.Price
 import christmas.model.VisitDate
@@ -11,13 +12,22 @@ class EventPlanner {
 
     fun startPlanner() {
         printHello()
+
         val visitDate = inputView.readDate()
         val order = inputView.readMenu()
         val price = Price(order)
         val discount = DecemberDiscount(visitDate, order, price)
         val outputView = OutputView(discount, visitDate, price)
-        printEventInfo(visitDate)
 
+        printEventInfo(visitDate)
+        printAllOutputView(outputView, order)
+    }
+
+    private fun printHello() = println(PLANNER_GREETING)
+
+    private fun printEventInfo(date: VisitDate) = println(EVENT_INFO.format(date.getVisitDate()))  // 이름 수정하기
+
+    private fun printAllOutputView(outputView: OutputView, order: Order) {
         with(outputView) {
             printMenu(order)
             printBeforeDiscountPrice()
@@ -28,10 +38,6 @@ class EventPlanner {
             printEventBadge()
         }
     }
-
-    private fun printHello() = println(PLANNER_GREETING)
-
-    private fun printEventInfo(date: VisitDate) = println(EVENT_INFO.format(date.getVisitDate()))  // 이름 수정하기
 
     companion object {
         private const val PLANNER_GREETING = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."
