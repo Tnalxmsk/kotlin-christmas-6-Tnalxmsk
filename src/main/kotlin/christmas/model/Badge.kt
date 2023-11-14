b17f768216b6be67966103c2e598ea09b9a35863
Tnalxmsk <alstnwkd990@naver.com>
2023-11-14 23:46:48 +0900

    fix(christmas): 혜택 금액이 5천원 이하여도 스타가 출력되는 버그 수정

--- src/main/kotlin/christmas/model/Badge.kt
+++ src/main/kotlin/christmas/model/Badge.kt
@@ -3,17 +3,20 @@ package christmas.model
 enum class Badge(val badgeName: String) {
     STAR("별"),
     TREE("트리"),
-    SANTA("산타");
+    SANTA("산타"),
+    NONE("없음");
 
     companion object {
         private const val SANTA_BADGE_AMOUNT = 20_000
         private const val TREE_BADGE_AMOUNT = 10_000
+        private const val STAR_BADGE_AMOUNT = 5000
 
         fun grantBadge(totalBenefit: Int): String {
             return when {
                 totalBenefit >= SANTA_BADGE_AMOUNT -> SANTA.badgeName
                 totalBenefit >= TREE_BADGE_AMOUNT -> TREE.badgeName
-                else -> STAR.badgeName
+                totalBenefit >= STAR_BADGE_AMOUNT -> STAR.badgeName
+                else -> NONE.badgeName
             }
         }
     }
