cab99e784043dd0d239475b1cd8cf261397a7e9d
Tnalxmsk <alstnwkd990@naver.com>
2023-11-15 20:29:17 +0900

    refactor(christmas): OutputView 메서드 추가
    EventPlanner 의 메서드인 printEventInfo()를 OutputView의 메서드로 변경

--- src/main/kotlin/christmas/EventPlanner.kt
+++ src/main/kotlin/christmas/EventPlanner.kt
@@ -19,16 +19,14 @@ class EventPlanner {
         val discount = DecemberDiscount(visitDate, order, price)
         val outputView = OutputView(discount, visitDate, price)
 
-        printEventInfo(visitDate)
         printAllOutputView(outputView, order)
     }
 
     private fun printHello() = println(PLANNER_GREETING)
 
-    private fun printEventInfo(date: VisitDate) = println(EVENT_INFO.format(date.getVisitDate()))  // 이름 수정하기
-
     private fun printAllOutputView(outputView: OutputView, order: Order) {
         with(outputView) {
+            printEventInfo()
             printMenu(order)
             printBeforeDiscountPrice()
             printPresentationMenu()
@@ -41,6 +39,5 @@ class EventPlanner {
 
     companion object {
         private const val PLANNER_GREETING = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."
-        private const val EVENT_INFO = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n" // 이름 수정
     }
 }
