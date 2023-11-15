11750cc7a7ed0a30401dfa11a2807d8a28727009
Tnalxmsk <alstnwkd990@naver.com>
2023-11-15 22:41:34 +0900

    refactor(christmas): 증정 이벤트 message 변경

--- src/main/kotlin/christmas/view/EventView.kt
+++ src/main/kotlin/christmas/view/EventView.kt
@@ -5,6 +5,6 @@ enum class EventView(val message: String) {
     WEEKDAY_DISCOUNT("평일 할인"),
     WEEKEND_DISCOUNT("주말 할인"),
     SPECIAL_DISCOUNT("특별 할인"),
-    PRESENTATION_DISCOUNT("증정 이벤트: -%,d원"),
+    PRESENTATION_EVENT("증정 이벤트"),
     PRESENT_GOODS("샴페인 1개\n"),
 }
