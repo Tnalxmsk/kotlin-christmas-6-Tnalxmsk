dbff6e52e2c746c2d3e50d226f632d674dd14118
Tnalxmsk <alstnwkd990@naver.com>
2023-11-15 23:44:46 +0900

    refactor(christmas): 매직 넘버 상수화

--- src/main/kotlin/christmas/validation/DateValidator.kt
+++ src/main/kotlin/christmas/validation/DateValidator.kt
@@ -7,12 +7,15 @@ enum class DateValidator(val error: String) {
     OUT_OF_RANGE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
 
     companion object {
+        private const val FIRST_DAY = 1
+        private const val LAST_DAY = 31
+
         fun validateDate(input: String) {
             val error = when {
                 input.isEmpty() -> EMPTY_DATE
                 input.contains(" ") -> INCLUDE_GAP
                 input.matches(Regex("\\d+")).not() -> INCLUDE_CHARACTER
-                input.toInt() !in (1..31) -> OUT_OF_RANGE
+                input.toInt() !in (FIRST_DAY..LAST_DAY) -> OUT_OF_RANGE
                 else -> return
             }
             throw IllegalArgumentException(error.error)
